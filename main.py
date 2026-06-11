b207779902a3ba767df4612f83f30a81b6da24aa
pradeesh-dev <pradeesh.analysis@gmail.com>
2026-06-11 18:21:13 +0530

    Workflow actions test commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world    ")
+import hashlib
+import sqlite3
+import subprocess
+
+OPENAI_API_KEY = "sk-test-12345678901234567890"  # pragma: allowlist secret
+
+user_input = input()
+
+subprocess.run(user_input, shell=True)
+
+query = f"SELECT * FROM users WHERE id = {user_input}"
+
+conn = sqlite3.connect("db.sqlite")
+conn.execute(query)
+
+hashlib.md5(user_input.encode())
